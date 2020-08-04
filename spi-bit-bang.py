48f6ceab6e60b77097e47e36cb67787e53077595
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2020-08-04 16:04:25 -0500

    SPI finally talking to the LMK (thank you God)

--- spi-bit-bang.py
+++ spi-bit-bang.py
@@ -1,8 +1,10 @@
 from pyftdi.gpio import GpioSyncController
+from pyftdi.gpio import GpioAsyncController
 import time
 
 class BitBangSPI:
-	def __init__(self, device, SCLK, MOSI, MISO, CS, defaultDirection=0xBB, defaultState=0x00):
+	def __init__(self, device, SCLK, MOSI, MISO, CS, defaultDirection, defaultState):
+		self.device = device
 		self.SCLK = SCLK
 		self.MOSI = MOSI
 		self.MISO = MISO
@@ -10,33 +12,33 @@ class BitBangSPI:
 		self.defaultDirection = defaultDirection
 		self.defaultState = defaultState
 		self.txArray = bytearray()
-		self.txArray.append(self.defaultState | self.CS)
+		self.txArray.append((self.defaultState | self.CS) & defaultDirection)
 		self.directionArray = bytearray()
 		self.directionArray.append(self.defaultDirection)
 		self.rxArray = bytearray()
-	def writeDelay(self, d, directionMask=0x00):
+	def insertDelay(self, d, directionMask=0xff):
 		for i in range(d):
 			self.txArray.append(self.txArray[-1])
-			self.directionArray.append(self.directionArray[-1] & ~directionMask)
-	def clockLow(self, directionMask=0x00):
+			self.directionArray.append(self.defaultDirection & directionMask)
+	def clockLow(self, directionMask=0xff):
 		self.txArray.append( self.txArray[-1] & ~self.SCLK )
-		self.directionArray.append(self.directionArray[-1] & ~directionMask)
-	def clockLowdataHigh(self, directionMask=0x00):
+		self.directionArray.append(self.defaultDirection & directionMask)
+	def clockLowdataHigh(self, directionMask=0xff):
 		self.txArray.append( (self.txArray[-1] & ~self.SCLK) | self.MOSI )
-		self.directionArray.append(self.directionArray[-1] & ~directionMask)
-	def clockLowdataLow(self, directionMask=0x00):
+		self.directionArray.append(self.defaultDirection & directionMask)
+	def clockLowdataLow(self, directionMask=0xff):
 		self.txArray.append( (self.txArray[-1] & ~self.SCLK) & ~self.MOSI )
-		self.directionArray.append(self.directionArray[-1] & ~directionMask)
-	def clockHigh(self, directionMask=0x00):
+		self.directionArray.append(self.defaultDirection & directionMask)
+	def clockHigh(self, directionMask=0xff):
 		self.txArray.append( self.txArray[-1] | self.SCLK )
-		self.directionArray.append(self.directionArray[-1] & ~directionMask)
-	def csLow(self, directionMask=0x00):
+		self.directionArray.append(self.defaultDirection & directionMask)
+	def csLow(self, directionMask=0xff):
 		self.txArray.append( self.txArray[-1] & ~self.CS )
-		self.directionArray.append(self.directionArray[-1] & ~directionMask)
-	def csHigh(self, directionMask=0x00):
+		self.directionArray.append(self.defaultDirection & directionMask)
+	def csHigh(self, directionMask=0xff):
 		self.txArray.append( self.txArray[-1] | self.CS )
-		self.directionArray.append(self.directionArray[-1] & ~directionMask)
-	def writeByte(self, aByte, directionMask=0x00):
+		self.directionArray.append(self.defaultDirection & directionMask)
+	def writeByte(self, aByte, directionMask=0xff):
 		for i in range(8):
 			shiftPlaces = 7-i # MSB first "big endian"
 			# clock falling edge and data transition
@@ -49,21 +51,29 @@ class BitBangSPI:
 	def getTxArray(self):
 		return self.txArray
 	def transmitSync(self, frequency=1e3):
-		gpio = GpioSyncController(device, self.direction, frequency)
-		gpio.configure()
+		gpio = GpioSyncController()
+		gpio.configure(self.device, self.directionArray[0], frequency=frequency)
 		self.rxArray = gpio.exchange( self.txArray );
 		gpio.close()
 	def transmitAsync(self, halfPeriod=0.005):
 		gpio = GpioAsyncController()
-		gpio.configure(device, self.directionArray[0])
+		gpio.configure(self.device, self.directionArray[0])
 		for i in range(len(self.txArray)):
 			if (self.directionArray[i] != self.directionArray[i-1]):
-				gpio.configure(device, self.directionArray[i])
-			# all output set high, apply direction mask
-			gpio.write(self.txArray[i] & gpio.direction)
+				time.sleep(halfPeriod)
+				gpio.close()
+				gpio = GpioAsyncController()
+				gpio.configure(self.device, self.directionArray[i])
+				time.sleep(halfPeriod)
+				#print("new direction: "+hex(self.directionArray[i]))
+			# read the state of the port
+			self.rxArray.append( gpio.read() )
+			#print("read: "+hex(self.rxArray[-1]))
+			# write to the outputs (0 is OK to write to inputs)
+			gpio.write(self.txArray[i])
+			#print("write: "+hex(self.txArray[i]))
+			# wait half a clock cycle
 			time.sleep(halfPeriod)
-		# all output forced to high, writing to input pins is illegal
-		gpio.write(0xFF)  # raises an IOError
 		gpio.close()
 	def getRxArray(self):
 		return self.rxArray
@@ -92,7 +102,7 @@ def printHex(bitBangArray):
 		print(hex(aByte))
 
 
-def readData(bitBangArray, clockMask, mosiMask, misoMask, directionArray=bytearray(), debug=1):
+def readBitBang(bitBangArray, clockMask, mosiMask, misoMask, directionArray=bytearray(), debug=0):
 	mosiArray = bytearray()
 	misoArray = bytearray()
 	bitPlace = 7
@@ -100,17 +110,17 @@ def readData(bitBangArray, clockMask, mosiMask, misoMask, directionArray=bytearr
 	misoByte = 0x00
 	for a in range(len(bitBangArray)):
 		if debug:
-			print("output= ", end='')
+			print("state= ", end='')
 			printByte(bitBangArray[a])
 			print(", direction= ", end='')
 			printByte(directionArray[a])
 		if (not (bitBangArray[a-1] & clockMask) and (bitBangArray[a] & clockMask)): # rising edge
 			if debug: print(", rising edge, bitPlace=" + str(bitPlace), end='')
 			if (bitBangArray[a] & mosiMask):
-				if debug: print(", mosi=1", end='')
+				if debug: print(", master=1", end='')
 				mosiByte += (1 << bitPlace)
 			if (bitBangArray[a] & misoMask): # data=1
-				if debug: print(", miso=1", end='')
+				if debug: print(", slave=1", end='')
 				misoByte += (1 << bitPlace)
 			bitPlace -= 1
 		if bitPlace < 0:
@@ -126,74 +136,93 @@ def readData(bitBangArray, clockMask, mosiMask, misoMask, directionArray=bytearr
 	return [mosiArray, misoArray]
 	
 	
-def formatData(txMOSI, txMISO, rxMOSI, rxMISO): #must all be same length
+def displayData(bb): #must all be same length
+	tx = readBitBang( bb.getTxArray(), bb.SCLK, bb.MOSI, bb.MISO, bb.getDirectionArray() )
+	rx = readBitBang( bb.getRxArray(), bb.SCLK, bb.MOSI, bb.MISO, bb.getDirectionArray() )
 	print(    "TX:         |  RX:")
 	print(    "MOSI  MISO  |  MOSI  MISO")
-	for i in range(len(txMOSI)):
-		print("0x{:02x}".format(txMOSI[i])+"  "+"0x{:02x}".format(txMISO[i])+"  |  "+"0x{:02x}".format(rxMOSI[i])+"  "+"0x{:02x}".format(rxMISO[i]))
+	for i in range(len(tx[0])):
+		print( \
+			"0x{:02x}".format(tx[0][i])+ \
+			"  "+"0x{:02x}".format(tx[1][i])+ \
+			"  |  "+"0x{:02x}".format(rx[0][i])+ \
+			"  "+"0x{:02x}".format(rx[1][i]) \
+			)
 	print()
 
 
-def spiTransaction(writeList, testing=1):
+def spiTransaction(transactionList):
 	sckMask = 0x10
 	sdioMask = 0x20
 	sdoMask = 0x40
 	csMask = 0x80
-	txBytes = BitBangSPI('ftdi:///2', sckMask, sdioMask, sdoMask, csMask) 
+	txBytes = BitBangSPI('ftdi:///2', sckMask, sdioMask, sdoMask, csMask, defaultDirection=0xBB, defaultState=0x88) 
 
-	### Convert writeList into txBytes array (bit-bang) ###
-	for aWrite in writeList:
-		txBytes.writeDelay(4)
+	### Convert transactionList into txBytes array (bit-bang) ###
+	for aTransaction in transactionList:
+		txBytes.insertDelay(4)
 		txBytes.clockLow()
 		txBytes.csLow()
 		# SPI control and address bytes
-		txBytes.writeByte(aWrite[0])
-		txBytes.writeByte(aWrite[1])
+		txBytes.writeByte(aTransaction[0])
+		txBytes.writeByte(aTransaction[1])
 		# Data byte
-		if (aWrite[0] & 0x80):
-			txBytes.writeByte(aWrite[2], directionMask=0x20)
-		else:
-			txBytes.writeByte(aWrite[2])
+		txBytes.writeByte(aTransaction[2])
+#		if (aTransaction[0] & 0x80):
+#			txBytes.writeByte(aTransaction[2], directionMask=(~0x20)) # reconfigure pin 0x20 as input
+#		else:
+#			txBytes.writeByte(aTransaction[2])
 		txBytes.csHigh()
 		txBytes.clockLow()
-		txBytes.writeDelay(4)
-		### SPI send and receive ###
-	if not testing:
-		#txBytes.transmitSync()
-		txBytes.transmitAsync
-	#print("directionArray:")
-	#printBin(txBytes.getDirectionArray())
-	return [\
-		readData(txBytes.getTxArray(), sckMask, sdioMask, sdoMask, directionArray=txBytes.getDirectionArray()),\
-		readData(txBytes.getRxArray(), sckMask, sdioMask, sdoMask, directionArray=txBytes.getDirectionArray())\
-		]
+		txBytes.insertDelay(4)
+	### SPI send and receive ###
+	txBytes.transmitSync(frequency=10000)
+	#txBytes.transmitAsync()
+	displayData( txBytes );
 
 
 
-writeList = [[0x00,0x00,0x10],\
-			[0x01,0x38,0x40],\
-			[0x01,0x3F,0x00],\
-			[0x01,0x40,0xF0],\
-			[0x01,0x47,0x00],\
-			[0x01,0x73,0x60]]
-			
-readList = [[0x00,0x00,0x10],\
-			[0x81,0x38,0x00],\
-			[0x81,0x3F,0x00],\
-			[0x81,0x40,0x00],\
-			[0x81,0x47,0x00],\
-			[0x81,0x73,0x00]]
 
-#writeList = [[0xCC,0xCC,0xCC],[0xAA,0xAA,0xAA]]
+write4wire =	[[0x00,0x00,0x10],\
+				[0x01,0x38,0x40],\
+				[0x01,0x3F,0x00],\
+				[0x01,0x40,0xF0],\
+				[0x01,0x47,0x00],\
+				[0x01,0x4A,0x33],\
+				[0x01,0x73,0x60]]
 
-#for i in range(100):
-writeResults = spiTransaction(writeList, 0); # write
 
-#readResults = spiTransaction(readList, 0); # read
+read4wire =		[[0x00,0x00,0x10],\
+				[0x81,0x38,0x00],\
+				[0x81,0x3F,0x00],\
+				[0x81,0x40,0x00],\
+				[0x81,0x47,0x00],\
+				[0x01,0x4A,0x00],\
+				[0x81,0x73,0x00]]
 
-#formatData(writeResults[0][0], writeResults[0][1], writeResults[1][0], writeResults[1][1])
-#formatData(readResults[0][0], readResults[0][1], readResults[1][0], readResults[1][1])
-	#time.sleep(.5)
+
+write3wire =	[[0x00,0x00,0x80],\
+				[0x01,0x38,0x40],\
+				[0x01,0x3F,0x00],\
+				[0x01,0x40,0xF0],\
+				[0x01,0x47,0x00],\
+				[0x01,0x73,0x60]]
+
+
+read3wire =		[[0x00,0x00,0x80],\
+				[0x81,0x38,0x00],\
+				[0x81,0x3F,0x00],\
+				[0x81,0x40,0x00],\
+				[0x81,0x47,0x00],\
+				[0x81,0x73,0x00]]
+
+
+
+
+for i in range(100):
+	spiTransaction(write4wire); # write
+	spiTransaction(read4wire); # read
+	time.sleep(.1)
 	
 
 

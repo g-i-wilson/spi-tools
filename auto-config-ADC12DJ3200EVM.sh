180ceb16e6795fc17ea9f677bdae67ff1c2ab8b0
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2020-10-16 10:43:48 -0500

    Added adc_config_SFORMAT.csv that has bit corrected

--- auto-config-ADC12DJ3200EVM.sh
+++ auto-config-ADC12DJ3200EVM.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-cd ~/spi-tools/
-printf "loadDefault\nloadCSV lmk_config.csv\nexit\n" | python3 ConfigureLMK_ADC12DJ3200EVM.py
-printf "loadDefault\nloadCSV adc_config.csv\nexit\n" | python3 ConfigureADC_ADC12DJ3200EVM.py
+printf "all\nloadCSV lmk_config.csv\nexit\n" | python3 ConfigureLMK_ADC12DJ3200EVM.py
+printf "all\nloadCSV adc_config_SFORMAT.csv\nexit\n" | python3 ConfigureADC_ADC12DJ3200EVM.py

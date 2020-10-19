ff370fc1625c2fb6b154a3a37e13433413288e8e
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2020-10-19 23:26:48 +0100

    edited python script to blink LED

--- auto-config-ADC12DJ3200EVM.py
+++ auto-config-ADC12DJ3200EVM.py
@@ -1,19 +1,26 @@
-from gpiozero import RGBLED
+from gpiozero import LED
+from signal import pause
+from datetime import datetime
+
 from time import sleep
 import os
 
 
-led = RGBLED(red=3, blue=5, green=7, active_high=False)
+red = LED(2, active_high=False)
+green = LED(4, active_high=False)
+blue = LED(3, active_high=False)
 
-led.color = (1, 1, 0)
-led.blink()
+red.blink()
+#sleep(10)
 
-os.system('printf "all\nloadCSV lmk_config.csv\nexit\n" | python3 ConfigureLMK_ADC12DJ3200EVM.py')
+
+os.system('printf "all\nloadCSV lmk_config.csv\nexit\n" | python3 ConfigureLMK_ADC12DJ3200EVM.py > lmk.log')
 sleep(5)
-os.system('printf "loadCSV lmx_config.csv\nexit\n" | python3 ConfigureLMX_ADC12DJ3200EVM.py')
+os.system('printf "loadCSV lmx_config.csv\nexit\n" | python3 ConfigureLMX_ADC12DJ3200EVM.py > lmx.log')
 sleep(5)
-os.system('printf "all\nloadCSV adc_config_SFORMAT.csv\nexit\n" | python3 ConfigureADC_ADC12DJ3200EVM.py')
+os.system('printf "all\nloadCSV adc_config_SFORMAT.csv\nexit\n" | python3 ConfigureADC_ADC12DJ3200EVM.py > adc.log')
 
-led.color = (0, 1, 0)
+red.off()
+green.on()
 
 pause()

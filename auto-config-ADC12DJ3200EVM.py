1c961a97572a366235f9f3b0517d8201fa8be371
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2021-06-16 18:30:18 +0100

    added blue LED during SPI writes

--- auto-config-ADC12DJ3200EVM.py
+++ auto-config-ADC12DJ3200EVM.py
@@ -15,17 +15,26 @@ blue = LED(3, active_high=False)
 red.blink()
 #sleep(10)
 
-
+blue.on()
 os.system('printf "all\nloadCSV lmk_config.csv\nexit\n" | python3 ConfigureLMK_ADC12DJ3200EVM.py > lmk.log')
-sleep(5)
-os.system('printf "loadCSV lmx_config.csv\nexit\n" | python3 ConfigureLMX_ADC12DJ3200EVM.py > lmx.log')
-sleep(5)
-os.system('printf "all\nloadCSV adc_config_SFORMAT.csv\nexit\n" | python3 ConfigureADC_ADC12DJ3200EVM.py > adc.log')
+blue.off()
 
+sleep(5)
+
+blue.on()
+os.system('printf "loadCSV lmx_config.csv\nexit\n" | python3 ConfigureLMX_ADC12DJ3200EVM.py > lmx.log')
+blue.off()
+
+sleep(5)
+
+blue.on()
+os.system('printf "all\nloadCSV adc_config_SFORMAT.csv\nexit\n" | python3 ConfigureADC_ADC12DJ3200EVM.py > adc.log')
+blue.off()
+
+sleep(5)
+
+comeOutOfReset.on()
 red.off()
 green.on()
-
-sleep(5)
-comeOutOfReset.on()
 
 pause()
